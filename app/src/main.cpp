31fa7a9d4f78602b2889dc915d651661dfd30e9e
Bernhard Reitinger <bernhard@reitinger.eu>
2025-08-19 15:08:26 +0200

    finish build

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 
 #include "core/CoreEngine.hpp"
+#include "core/io/Export.hpp"
+#include "io/Project.hpp"
 
 #ifdef ENABLE_COSMA
 #include "cosma/CosmaViewer.hpp"
@@ -31,17 +33,40 @@ static bool ParseArgs(int argc, char** argv, Cmd& cmd) {
     if (argc < 3) return false;
     cmd.command = argv[1];
     cmd.projectFile = argv[2];
-    // for (int i = 3; i < argc; ++i) {
-    //     std::string a = argv[i];
-    //     if ((a == "-o" || a == "--out") && i + 1 < argc) {
-    //         cmd.outdir = argv[++i];
-    //     } else {
-    //         std::cerr << "Unknown arg: " << a << "\n";
-    //         return false;
-    //     }
-    // }
 
     return cmd.command == "build" || cmd.command == "live";
+}
+
+static bool BuildProject(const std::string& projectFile, std::unique_ptr<CoreEngine> engine,
+                         const fs::path& projectOutputPath) {
+    auto project = io::LoadProject(projectFile);
+    io::PrintProject(project);
+    auto projectRoot = std::filesystem::absolute(std::filesystem::path(projectFile)).parent_path();
+
+    auto outDir = projectRoot / projectOutputPath;
+    fs::create_directory(outDir);
+
+    for (const auto& jp : project.parts) {
+        std::filesystem::path src = projectRoot / jp.source;
+        auto luaFile = std::filesystem::weakly_canonical(src);
+
+        std::string err;
+        if (!engine->RunFile(luaFile, &err)) {
+            auto errStr = std::string("Lua error in ") + luaFile.string() + ": " + err;
+            std::cerr << errStr << std::endl;
+            return false;
+        }
+
+        auto emitted = engine->GetEmitted();
+        if (!emitted) {
+            std::cerr << "Canot get shape from " << luaFile << std::endl;
+            return false;
+        }
+        const auto stem = fs::path(luaFile).stem().string();
+        const fs::path stlFile = outDir / (stem + ".stl");
+        io::SaveSTL(emitted.value(), stlFile.string(), 0.1);
+    }
+    return true;
 }
 
 int main(int argc, char** argv) {
@@ -51,8 +76,7 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    cmd.outDir = "out";  // as parameter?
-    fs::create_directory(cmd.outDir);
+    cmd.outDir = "generated";  // as parameter?
 
     std::string libRoot = "/Users/breiting/workspace/codecad/lib";
     std::string path = libRoot + "/?.lua;" + libRoot + "/?/init.lua;";
@@ -71,9 +95,12 @@ int main(int argc, char** argv) {
         if (viewer) {
             viewer->start(cmd.projectFile, std::move(engine));
         } else {
-            std::cout << "Viewer support is not available in this build.\n";
+            std::cerr << "Viewer support is not available in this build.\n";
         }
     } else if (cmd.command == "build") {
+        if (!BuildProject(cmd.projectFile, std::move(engine), cmd.outDir)) {
+            std::cerr << "Error during file generation." << std::endl;
+        }
     }
 
     return 0;
