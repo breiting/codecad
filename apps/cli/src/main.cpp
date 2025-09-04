2489737279ad749efa6923b084fcf34f59e18d1b
Bernhard Reitinger <bernhard@reitinger.eu>
2025-09-04 17:12:23 +0200

    Introduce versioning

--- apps/cli/src/main.cpp
+++ apps/cli/src/main.cpp
@@ -1,6 +1,11 @@
+#include <ccad/version.hpp>
+#include <iostream>
+
 #include "App.hpp"
+using namespace std;
 
 int main(int argc, char** argv) {
+    cout << "CodeCAD Kernel Version: " << CODECAD_VERSION_STRING << endl;
     App a;
     a.start(argc, argv);
     return 0;
